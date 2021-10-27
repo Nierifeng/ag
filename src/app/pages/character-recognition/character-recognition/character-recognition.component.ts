02ce81bbe43656bcaa80ef4ca70d8cc0b52f6896
Nick <nick@cxist.com>
2021-10-27 21:08:25 +0800

    rectify character recognition

--- src/app/pages/character-recognition/character-recognition/character-recognition.component.ts
+++ src/app/pages/character-recognition/character-recognition/character-recognition.component.ts
@@ -1,7 +1,12 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
+
+interface ImgFile {
+    file: File,
+    name: string,
+    url: string,
+}
 
 @Component({
     selector: 'app-character-recognition',
@@ -14,19 +19,10 @@ export class CharacterRecognitionComponent implements OnInit {
     public imgSize?: number;
 
     @Input()
-    public multiple: boolean = false;
-
-    @Input()
     public isCanUpload: boolean = true;
 
     @Input()
     public maxImgNumber?: number;
-
-    @Input()
-    public isDownloadable: boolean = false;
-
-    @Input()
-    public isDeletable: boolean = true;
 
     @Input()
     public imgPreviewSize: number = 900;
@@ -72,77 +68,11 @@ export class CharacterRecognitionComponent implements OnInit {
 
     public async onFileSelect(event: Event): Promise<void> {
         let selectFileList: FileList | null = (event.target as HTMLInputElement).files;
-        let selectFiles: Array<FileUpload> = new Array<FileUpload>();
-        if (!selectFileList) {
-            return;
-        }
-        for (let i: number = 0; i < selectFileList.length; i++) {
-            if (this.maxImgNumber && this.imgFiles.length == this.maxImgNumber - 1) {
-                this.isCanUpload = false;
-            }
-            if (!this.verifying(selectFileList[i])) {
-                return;
-            }
-            let file: ImgFile = { file: selectFileList[i], url: this.sanitizer.bypassSecurityTrustUrl((window.URL.createObjectURL(selectFileList[i]))) };
-
-            let fileManagement: FileManagement = {
-                id: '',
-                fileType: selectFileList[i].type,
-                fileSize: this.fileManagementService.formatSize(selectFileList[i].size),
-                resourceId: this.resourceId,
-                resourceKey: this.resourceKey,
-            };
-            fileManagement.fileName = file.name = selectFileList[i].name;
-            let resfileManagement: FileManagement = await this.fileManagementService.createAsync(fileManagement);
-            if (resfileManagement.uploadUrl) {
-                (await this.fileManagementService.upload(resfileManagement.uploadUrl, file.file!)).subscribe(async (e: HttpEvent<any>) => {
-                    if (e.type == HttpEventType.Response) {
-                        await this.fileManagementService.updateStatus(resfileManagement.id);
-                    }
-                });
-                file.isImg = true;
-                selectFiles.push(file);
-                this.imgFiles.push(file);
-            }
-        }
-        this.onSelect.emit({ originalEvent: event, files: this.imgFiles, currentFiles: selectFiles });
     }
 
     public toView(index: number): void {
         this.activeIndex = index;
         this.displayCustom = true;
-    }
-
-    public async download(id: string): Promise<void> {
-        const downloadUrl: string | null = await this.fileManagementService.getDownloadUrl(id);
-        if (downloadUrl) {
-            let a: HTMLAnchorElement = document.createElement('a');
-            let e: MouseEvent = document.createEvent('MouseEvents');
-            e.initEvent('click', false, false);
-            a.href = downloadUrl;
-            a.dispatchEvent(e);
-            a.remove();
-        }
-    }
-
-    public async remove(index: number, id?: string): Promise<void> {
-        this.onRemove.emit({ removeFile: this.imgFiles[index] });
-        if (id) {
-            await this.resService.deleteAsync(this.resourceName, id);
-        }
-        if (this.imgFiles && this.imgFiles.length > 0) {
-            this.imgFiles.splice(index, 1);
-        }
-        if (this.maxImgNumber && this.imgFiles.length < this.maxImgNumber) {
-            this.isCanUpload = true;
-        }
-    }
-
-    public whetherUploadComplete(): boolean {
-        if (this.imgFiles && this.imgFiles.filter(item => item.id === undefined).length > 0) {
-            return false;
-        }
-        return true;
     }
 
     public async refresh(resourceId: string): Promise<void> {
