a94be4110f848656c5552855f7d9c15f932b2317
Nierifeng <50561965+Nierifeng@users.noreply.github.com>
2021-10-28 16:57:45 +0800

    remove unnecessary code (#8)
    Co-authored-by: Nick <nick@cxist.com>

--- src/app/pages/character-recognition/character-recognition.module.ts
+++ src/app/pages/character-recognition/character-recognition.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzUploadModule } from 'ng-zorro-antd/upload';
-import { CharacterRecognitionComponent } from './character-recognition/character-recognition.component';
 import { CharacterRecognitionRoutingModule } from './character-recognition/character-recognition-routing.module';
+import { CharacterRecognitionComponent } from './character-recognition/character-recognition.component';
 
 
 
 @NgModule({
 
-    imports: [NzUploadModule, CharacterRecognitionRoutingModule, CommonModule],
+    imports: [NzUploadModule, CharacterRecognitionRoutingModule, CommonModule, NzIconModule],
     exports: [CharacterRecognitionComponent],
     declarations: [
         CharacterRecognitionComponent
