5874b3f577f5f45ab794c6d038336f6e85bc59cd
Nick <nick@cxist.com>
2021-10-28 16:53:34 +0800

    remove unnecessary code

--- src/app/pages/character-recognition/character-recognition/character-recognition.component.ts
+++ src/app/pages/character-recognition/character-recognition/character-recognition.component.ts
@@ -15,42 +15,15 @@ interface ImgFile {
 })
 export class CharacterRecognitionComponent implements OnInit {
 
-    @Input()
     public imgSize?: number;
 
-    @Input()
     public isCanUpload: boolean = true;
-
-    @Input()
-    public maxImgNumber?: number;
-
-    @Input()
-    public imgPreviewSize: number = 900;
-
-    @Input()
-    public resourceKey: string = '';
-
-    @Input()
-    public resourceId: string = '0';
-
-    @Output()
-    public readonly onSelect: EventEmitter<any> = new EventEmitter();
-
-    @Output()
-    public readonly onRemove: EventEmitter<any> = new EventEmitter();
-
-    @Output()
-    public readonly onError: EventEmitter<any> = new EventEmitter();
 
     public accept: string = 'image/*';
 
-    public activeIndex?: number;
-
     public displayCustom: boolean = false;
 
-    public imgFiles: Array<ImgFile> = [];
-
-    public resourceName: string = 'basis/filestorage/FileManagement';
+    public imgFile?: ImgFile;
 
     @ViewChild('file', { static: false })
     public file?: ElementRef;
@@ -69,40 +42,4 @@ export class CharacterRecognitionComponent implements OnInit {
     public async onFileSelect(event: Event): Promise<void> {
         let selectFileList: FileList | null = (event.target as HTMLInputElement).files;
     }
-
-    public toView(index: number): void {
-        this.activeIndex = index;
-        this.displayCustom = true;
-    }
-
-    public async refresh(resourceId: string): Promise<void> {
-    }
-
-    private isSameFileName(file: File): boolean {
-        for (let img of this.imgFiles) {
-            if (img.file && img.name === file.name) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private verifying(file: File): boolean {
-        if (this.isSameFileName(file)) {
-            this.onError.emit({ error: { no: '1', value: 'Duplicate file names' } });
-            return false;
-        }
-
-        if (this.imgSize && file.size > this.imgSize * 1024) {
-            this.onError.emit({ error: { no: '2', value: 'Over the maximum upload size' } });
-            return false;
-        }
-
-        if (this.maxImgNumber && this.maxImgNumber < this.imgFiles.length + 1) {
-            this.onError.emit({ error: { no: '3', value: 'Exceed maximum quantity' } });
-            return false;
-        }
-        return true;
-    }
-
 }
